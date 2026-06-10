16a0674e34dd3a30a83376998ec9236b7856378e
balajiuthayakumar08-ship-it <balajiuthayakumar08@gmail.com>
2026-06-10 14:04:20 +0530

    Create app.py

---
+++ TASK3/app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from PIL import Image
+from transformers import pipeline
+
+st.set_page_config(page_title="Image Captioning AI")
+
+st.title("🖼️ Image Captioning AI")
+
+uploaded_file = st.file_uploader(
+    "Upload an Image",
+    type=["jpg", "jpeg", "png"]
+)
+
+if uploaded_file:
+    image = Image.open(uploaded_file)
+
+    st.image(image, caption="Uploaded Image", use_container_width=True)
+
+    with st.spinner("Generating Caption..."):
+        captioner = pipeline(
+            "image-to-text",
+            model="Salesforce/blip-image-captioning-base"
+        )
+
+        result = captioner(image)
+
+    st.success("Caption Generated!")
+    st.write("### Caption:")
+    st.write(result[0]["generated_text"])
